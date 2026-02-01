1c17d10935c1bf77c021fc64bf3577157cae003a
Rahul Arutla <arutlarahul1@gmail.com>
2026-02-01 20:32:46 +0530

    fix double server listen

--- server/server.js
+++ server/server.js
@@ -5,31 +5,22 @@ const mongoose = require("mongoose");
 const menuRoutes = require("./routes/menuRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 
-
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/menu", menuRoutes);
 app.use("/api/orders", orderRoutes);
 
-
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch(err => console.log(err));
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-;
